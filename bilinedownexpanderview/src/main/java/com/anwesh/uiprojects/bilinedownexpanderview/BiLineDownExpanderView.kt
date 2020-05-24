1f32cb62449d4e363d4800459e9e4d1c136a83dd
Anwesh43 <anweshthecool0@gmail.com>
2020-05-25 05:08:04 +0530

    adding Canvas extension method

--- bilinedownexpanderview/src/main/java/com/anwesh/uiprojects/bilinedownexpanderview/BiLineDownExpanderView.kt
+++ bilinedownexpanderview/src/main/java/com/anwesh/uiprojects/bilinedownexpanderview/BiLineDownExpanderView.kt
@@ -12,10 +12,9 @@ import android.graphics.Paint
 import android.graphics.Canvas
 import android.graphics.Color
 
-val nodes : Int = 5
 val lines : Int = 2
 val parts : Int = 2
-val scGap : Float = 0.02f
+val scGap : Float = 0.04f / (lines * parts)
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 2.9f
 val colors : Array<String> = arrayOf("#3F51B5", "#F44336", "#4CAF50", "#03A9F4", "#009688")
@@ -26,3 +25,33 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawBiLineDownExpanderLine(i : Int, scale : Float, size : Float, paint : Paint) {
+    val sf1 : Float = scale.divideScale(0, parts)
+    val sf2 : Float = scale.divideScale(1, parts)
+    val si : Float = 1f - 2 * i
+    save()
+    translate(0f, size * si * (1 - sf2))
+    drawLine(0f, 0f, size * sf1 * si, 0f, paint)
+    restore()
+}
+
+fun Canvas.drawBiLineDownExpanderLines(scale : Float, size : Float, paint : Paint) {
+    for (i in 0..(lines - 1)) {
+        val sf : Float = scale.sinify().divideScale(i, lines)
+        drawBiLineDownExpanderLine(i, sf, size, paint)
+    }
+}
+
+fun Canvas.drawBLDENode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val size : Float = Math.min(w, h) / sizeFactor
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    save()
+    translate(w / 2, h / 2)
+    drawBiLineDownExpanderLines(scale, size, paint)
+    restore()
+}
